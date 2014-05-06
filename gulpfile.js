5159a5fb78b83d34262a656d2efa77c8bcf12056
Måns Hommerberg <mans.hommerberg@gmail.com>
2014-05-06 21:06:14 +0200

    Adding error handling with gulp-notify
    Also added another client module.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
+//TODO: Split the components up into some modules
+
 var gulp = require("gulp"),
     livereload = require("gulp-livereload"),
     browserify = require("browserify"),
     jade = require("gulp-jade"),
     embedlr = require("gulp-embedlr"),
-    source = require("vinyl-source-stream");
+    source = require("vinyl-source-stream"),
+    notify = require("gulp-notify");
 
 var pageOptions = require("./mypage.json");
 
+function getErrorHandler(title) {
+    return notify.onError({
+        message: "<%= error.message %>",
+        title: "ERROR: " + title
+    });
+}
+
 gulp.task("browserify", function () {
-    browserify('./javascript/index.js')
+    browserify("./javascript/index.js")
         .bundle({debug: true})
-        .pipe(source('bundle.js'))
+        .on("error", getErrorHandler("browserify"))
+        .pipe(source("bundle.js"))
         .pipe(gulp.dest("./out/"));
 });
 
@@ -19,6 +30,7 @@ gulp.task("jade", function () {
         .pipe(jade({
             locals: pageOptions
         }))
+        .on("error", getErrorHandler("jade"))
         .pipe(embedlr())
         .pipe(gulp.dest("./out/"));
 });
@@ -31,4 +43,4 @@ gulp.task("watch", function () {
     });
 });
 
-gulp.task("default", ["watch"]);
+gulp.task("default", ["browserify", "jade", "watch"]);
--- javascript/requestAnimationFrame.js
+++ javascript/requestAnimationFrame.js
@@ -1,5 +1,5 @@
 module.exports = window.requestAnimationFrame ||
-	window.webkitRequestAnimationFrame ||
-	window.mozRequestAnimationFrame ||
-	window.msRequestAnimationFrame ||
-	function (cb) { setTimeout(cb, 1000 / 60); };
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (cb) { setTimeout(cb, 1000 / 60); };
