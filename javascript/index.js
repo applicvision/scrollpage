36ceef698daf5695bfe3ac955ee4e119c7b72372
Måns Hommerberg <mans.hommerberg@gmail.com>
2014-05-09 20:22:06 +0200

    Better and separated css, removing non used js.
    Sections are full screen by adding another wrapper div, and header
    looks nicer. Color specification moved to style.css. Non used resize
    listener removed from js.

--- javascript/index.js
+++ javascript/index.js
@@ -11,7 +11,6 @@ var sections;
 var sectionLinks;
 var lastSectionLink;
 var icon;
-var sectionHeight;
 var background;
 var shouldParallax;
 
@@ -25,33 +24,17 @@ function scrollHandler () {
     }
 }
 
-function resizeSections () {
-    for (var i = 0; i < sections.length; i++) {
-        sections[i].style.height = windowHeight;
-    }
-    isResizing = false;
-}
-
-function resizeHandler () {
-    console.log("resize");
-    windowHeight = window.innerHeight;
-    if (!isResizing) {
-        requestAnimationFrame(resizeSections);
-        isResizing = true;
-    }
-}
-
 function update () {
     var currentSection = getCurrentSection(lastScrollY);
 
     if (currentSection !== lastSection) {
-        // header.classList.remove("show");
+        var last = sectionLinks[lastSection];
+        if (last) {
+            console.log("removing last");
+            last.classList.remove("current");
+        }
         if (currentSection !== -1) {
             // header.classList.add("show");
-            var last = sectionLinks[lastSection];
-            if (last) {
-                last.classList.remove("current");
-            }
             var current = sectionLinks[currentSection];
             if (current) {
                 current.classList.add("current");
@@ -73,19 +56,17 @@ function update () {
 }
 
 function getCurrentSection(scrollOffset) {
-    return Math.floor(Math.abs(scrollOffset + sectionHeight / 3) / sectionHeight) - 1;
+    var sectionHeight = window.innerHeight;
+    return Math.floor(Math.abs(scrollOffset + sectionHeight / 4) / sectionHeight) - 1;
 }
 
 window.onload = function () {
     icon = document.querySelector(".iconholder");
     header = document.querySelector(".header");
     sectionLinks = header.querySelectorAll("a");
-    sections = document.querySelectorAll("section");
     background = document.querySelector(".background");
     shouldParallax = background.classList.contains("parallax");
-    sectionHeight = document.querySelector("section").clientHeight;
 
     window.onscroll = scrollHandler;
-    window.onresize = resizeHandler;
 };
 
