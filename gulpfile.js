b205b4d93177bf4d6c6eec78a9ecc5e854e480a1
Måns Hommerberg <mans.hommerberg@gmail.com>
2014-05-14 09:05:58 +0200

    Adding dist gulp task. Abandoning the out directory in favour of one dev and one dist directory. Generated index.html in top directory is dev version, index.dist.html is dist version. Watches redirected accordingly. All images now live in the images directory, but "images/" should not be included in image names in json file.

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,36 @@ gulp.task("browserify", function () {
         .bundle({debug: true})
         .on("error", getErrorHandler("browserify"))
         .pipe(source("bundle.js"))
-        .pipe(gulp.dest("./out/"));
+        .pipe(gulp.dest("./dev/"));
+});
+
+gulp.task("browserify-dist", function () {
+    browserify("./javascript/index.js")
+        .bundle()
+        .on("error", getErrorHandler("browserify"))
+        .pipe(source("bundle.min.js"))
+        .pipe(streamify(uglify()))
+        .pipe(gulp.dest("./dist/"));
+});
+
+//TODO: Maybe consolidate all dist tasks, and embed streams in jade template, instead of writing files.
+gulp.task("jade-dist", function () {
+    var locals = pageOptions;
+    locals.dist = true;
+    gulp.src("templates/**/*.jade")
+        .pipe(jade({
+            locals: locals
+        }))
+        .on("error", getErrorHandler("jade"))
+        .pipe(concat("index.dist.html"))
+        .pipe(gulp.dest("./"));
+});
+
+gulp.task('minify-css', function() {
+  gulp.src('./css/*.css')
+    .pipe(minifyCSS())
+    .pipe(concat("all.min.css"))
+    .pipe(gulp.dest('./dist/'))
 });
 
 gulp.task("jade", function () {
@@ -32,15 +61,18 @@ gulp.task("jade", function () {
         }))
         .on("error", getErrorHandler("jade"))
         .pipe(embedlr())
-        .pipe(gulp.dest("./out/"));
+        .pipe(gulp.dest("./"));
 });
 
 gulp.task("watch", function () {
     gulp.watch("javascript/**/*.js", ["browserify"]);
+
+    //TODO: Fix so that jade task is run whenever json file changes
     gulp.watch("templates/**/*.jade", ["jade"]);
-    gulp.watch("out/**").on("change", function (file) {
+    gulp.watch(["index.html", "dev/**", "css/**", "images/**"]).on("change", function (file) {
         livereload().changed(file.path);
     });
 });
 
 gulp.task("default", ["browserify", "jade", "watch"]);
+gulp.task("dist", ["browserify-dist", "minify-css", "jade-dist"])
